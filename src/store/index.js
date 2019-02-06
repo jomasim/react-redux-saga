999b80cad66718ff9a63a39369a136146bc4f5e9
warachi <morriswarachi0@gmail.com>
2019-02-06 17:32:48 +0300

    chore(redux): Redux setup
    - install redux and redux-saga
    - create necessary files for the sagas setup
    
    [Delivers #162949029]

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootreducer from './rootReducer';
+
+import rootSaga from '../sagas';
+
+const configureStore = () => {
+    const sagaMiddleware = createSagaMiddleware();
+    return {
+        ...createStore(
+            rootreducer,
+            applyMiddleware(sagaMiddleware)),
+            runSaga: sagaMiddleware.run(rootSaga)
+    }
+}
+
+export default configureStore
---
+++ src/store/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+import app from "../containers/app/reducer";
+
+const rootreducer = combineReducers({
+  app
+});
+
+export default rootreducer;
